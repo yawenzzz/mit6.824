c0bc377ae5b2c33d3b5f6ca1f72ddad1bcd5e191
Yawen Zheng <yawenzheng00@gmail.com>
2023-08-23 22:06:38 +0800

    lab1

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,33 +1,220 @@
 package mr
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+// 定义状态常量用于标识worker的状态
+type State int
 
+const (
+	Map State = iota
+	Reduce
+	Wait
+	Exited
+)
+
+// 定义任务的状态
+// 状态只有两个，要么是完成，要么就是进行中
+type TaskState int
+
+const (
+	Completed TaskState = iota
+	InProgress
+	Idle
+)
+
+type Task struct {
+	Taskid        int
+	InputFile     string
+	OutputFile    string // 最终的输出文件的列表
+	NReduce       int
+	Intermediates []string // worker mapping的中间文件
+	State         State
+}
+
+// 记录Task的信息
+type TaskInfo struct {
+	TaskState TaskState
+	StartTime time.Time // 记录任务开始的时间，用来应对crashed
+	ThisTask  *Task
+}
 type Master struct {
 	// Your definitions here.
-
+	TaskQueue     chan *Task        //待处理的任务队列 储存的是指向Task的指针
+	TaskData      map[int]*TaskInfo // k: id v: TaskInfo
+	MasterState   State
+	NReducer      int
+	InputFiles    []string //输入文件的序列
+	Intermediates [][]string
 }
+
+var mux sync.Mutex
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (m *Master) catchTimeOut() {
+	for {
+		fmt.Println("catching timeout...")
+		time.Sleep(10 * time.Second)
+		mux.Lock()
+		fmt.Println(time.Now())
+		fmt.Println("State of master: ", m.MasterState)
+		fmt.Println("length of Task: ", len(m.TaskQueue))
+		if m.MasterState == Exited {
+			mux.Unlock()
+			return
+		}
+		for _, taskInfo := range m.TaskData {
+			if taskInfo.TaskState == InProgress && time.Now().Sub(taskInfo.StartTime) > 10*time.Second {
+				m.TaskQueue <- taskInfo.ThisTask
+				taskInfo.TaskState = Idle
+			}
+		}
+		mux.Unlock()
+	}
+}
 
-//
+/*
+	func (m *Master) catchTimeOut() {
+		for {
+			fmt.Println("catching timeout...")
+			time.Sleep(1 * time.Second)
+			mux.Lock()
+			fmt.Println(time.Now())
+			fmt.Println("State of master: ", m.MasterState)
+			fmt.Println("length of Task: ", len(m.TaskQueue))
+			if m.MasterState == Map {
+				for _, taskinfo := range m.TaskData {
+					// 如果超时则需要重新等待分配任务
+					if taskinfo.TaskState == InProgress && time.Now().Sub(taskinfo.StartTime) > 3*time.Second {
+						m.TaskQueue <- taskinfo.ThisTask
+						taskinfo.TaskState = Idle
+					}
+				}
+			} else if m.MasterState == Reduce {
+				for _, taskinfo := range m.TaskData {
+					// 如果超时则需要重新等待分配任务
+					if taskinfo.TaskState == InProgress && time.Now().Sub(taskinfo.StartTime) > 3*time.Second {
+						m.TaskQueue <- taskinfo.ThisTask
+						taskinfo.TaskState = Idle
+					}
+				}
+			} else if m.MasterState == Exited {
+				mux.Unlock()
+				return
+			}
+		}
+	}
+*/
+func (m *Master) createMapTask() {
+	for index, filename := range m.InputFiles {
+		task := Task{
+			InputFile: filename,
+			Taskid:    index,
+			NReduce:   m.NReducer,
+			State:     Map,
+		}
+		m.TaskQueue <- &task
+		m.TaskData[index] = &TaskInfo{
+			TaskState: Idle,
+			ThisTask:  &task,
+		}
+	}
+}
+
+func (m *Master) createReduceTask() {
+	m.TaskData = make(map[int]*TaskInfo)
+	for index, files := range m.Intermediates {
+		task := Task{
+			Taskid:        index,
+			NReduce:       m.NReducer,
+			State:         Reduce,
+			Intermediates: files,
+		}
+		m.TaskQueue <- &task
+		m.TaskData[index] = &TaskInfo{
+			TaskState: Idle,
+			ThisTask:  &task,
+		}
+	}
+}
+
+// worker通知master任务已结束
+func (m *Master) TaskCompleted(task *Task, reply *ExampleReply) error {
+	mux.Lock()
+	defer mux.Unlock()
+	if task.State != m.MasterState || m.TaskData[task.Taskid].TaskState == Completed {
+		return nil
+	}
+	m.TaskData[task.Taskid].TaskState = Completed
+	go m.processTaskResult(task)
+	return nil
+}
+
+// 给worker分配任务
+func (m *Master) AssignTask(args *ExampleArgs, reply *Task) error {
+	mux.Lock()
+	defer mux.Unlock()
+	// 检查消息队列是否有任务待分配
+	if len(m.TaskQueue) > 0 {
+		*reply = *<-m.TaskQueue
+		// 更新状态
+		m.TaskData[reply.Taskid].TaskState = InProgress
+		m.TaskData[reply.Taskid].StartTime = time.Now()
+	} else if m.MasterState == Exited {
+		*reply = Task{State: Exited}
+	} else {
+		*reply = Task{State: Wait}
+	}
+	return nil
+}
+
+func (m *Master) processTaskResult(task *Task) {
+	mux.Lock()
+	defer mux.Unlock()
+	switch task.State {
+	case Map:
+		for reduceTaskId, filePath := range task.Intermediates {
+			m.Intermediates[reduceTaskId] = append(m.Intermediates[reduceTaskId], filePath)
+		}
+		if m.allTaskDone() {
+			m.createReduceTask()
+			m.MasterState = Reduce
+		}
+	case Reduce:
+		if m.allTaskDone() {
+			m.MasterState = Exited
+		}
+	}
+}
+
+// 判断所有的task是否完成，如果全部完成则返回true
+func (m *Master) allTaskDone() bool {
+	for _, task := range m.TaskData {
+		if task.TaskState != Completed {
+			return false
+		}
+	}
+	return true
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (m *Master) server() {
 	rpc.Register(m)
 	rpc.HandleHTTP()
@@ -41,30 +228,35 @@ func (m *Master) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrmaster.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (m *Master) Done() bool {
-	ret := false
-
 	// Your code here.
-
+	mux.Lock()
+	defer mux.Unlock()
+	ret := m.MasterState == Exited
 
 	return ret
 }
 
-//
 // create a Master.
 // main/mrmaster.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
-
 	// Your code here.
+	fmt.Println("Master ready...")
+	m := Master{
+		TaskQueue:     make(chan *Task, max(nReduce, len(files))),
+		TaskData:      make(map[int]*TaskInfo),
+		MasterState:   Map,
+		NReducer:      nReduce,
+		InputFiles:    files,
+		Intermediates: make([][]string, nReduce),
+	}
 
-
+	m.createMapTask()
+	fmt.Println("created map tasks!")
 	m.server()
+	go m.catchTimeOut()
 	return &m
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,180 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func askForTask() Task {
+	args := ExampleArgs{}
+	reply := Task{}
+	call("Master.AssignTask", &args, &reply)
+	return reply
+}
 
-//
-// main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the master.
-	// CallExample()
+// mapping过程
+func mapping(task *Task, mapf func(string, string) []KeyValue) {
+	file, err := os.Open(task.InputFile)
+	if err != nil {
+		log.Fatalf("cannot open %v", task.InputFile)
+	}
+	//读取文件的内容
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", task.InputFile)
+	}
+	file.Close()
+	// 中间态, 缓存结果
+	intermediates := mapf(task.InputFile, string(content))
+	// 将缓存后的结果写入本地，并且分成R份
+	// buffer[NReducer][KeyValue]
+	buffer := make([][]KeyValue, task.NReduce)
+	for _, intermediate := range intermediates {
+		// hash(key) mod R
+		split := ihash(intermediate.Key) % task.NReduce
+		buffer[split] = append(buffer[split], intermediate)
+	}
+	outputfiles := make([]string, 0)
+	for i := 0; i < task.NReduce; i++ {
+		outputfiles = append(outputfiles, writeToLocalFile(task.Taskid, i, &buffer[i]))
+	}
+	task.Intermediates = outputfiles
+	reply := ExampleReply{}
+	call("Master.TaskCompleted", task, &reply)
 
 }
 
-//
+func reducing(task *Task, reducef func(string, []string) string) {
+	intermediate := *readLocalFile(task.Intermediates)
+
+	sort.Sort(ByKey(intermediate))
+
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Fail to create temp file", err)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+	tempFile.Close()
+	oname := fmt.Sprintf("mr-out-%d", task.Taskid+1)
+	os.Rename(tempFile.Name(), oname)
+	task.OutputFile = oname
+	reply := ExampleReply{}
+	call("Master.TaskCompleted", task, &reply)
+
+}
+
+// 返回文件路径 dir+outputname
+func writeToLocalFile(taskid int, ireducer int, kva *[]KeyValue) string {
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create tempfile ", err)
+	}
+	enc := json.NewEncoder(tempFile)
+	for _, kv := range *kva {
+		if err := enc.Encode(&kv); err != nil {
+			log.Fatal("Failed to write kv pair", err)
+		}
+	}
+	tempFile.Close()
+	outputName := fmt.Sprintf("mr-%d-%d", taskid, ireducer)
+	os.Rename(tempFile.Name(), outputName)
+	return filepath.Join(dir, outputName)
+}
+
+func readLocalFile(files []string) *[]KeyValue {
+	kva := []KeyValue{}
+	for _, filepath := range files {
+		file, err := os.Open(filepath)
+		if err != nil {
+			log.Fatal("Fail to open file "+filepath, err)
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	return &kva
+}
+
+// for sorting by key.
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+// main/mrworker.go calls this function.
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	//fmt.Println("Worker ready...")
+	// Your worker implementation here.
+	for {
+		task := askForTask()
+		switch task.State {
+		case Map:
+			fmt.Println("Mapping...")
+			mapping(&task, mapf)
+		case Reduce:
+			fmt.Println("Reducing...")
+			reducing(&task, reducef)
+		case Wait:
+			fmt.Println("Waiting...")
+			time.Sleep(10 * time.Second)
+		case Exited:
+			fmt.Println("Finished!")
+			return
+		}
+	}
+}
+
 // example function to show how to make an RPC call to the master.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -61,11 +193,10 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
-//
 // send an RPC request to the master, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
+// rpcname是远程函数名
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := masterSock()
